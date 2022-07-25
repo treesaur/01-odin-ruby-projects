d973b847e7e4b5271a35ff8a7f9a6c9fcb1dd22f
rly <rallenly@gmail.com>
2022-07-25 10:10:28 -0400

    completed stock_picker

--- stock_picker.rb
+++ stock_picker.rb
@@ -1,3 +1,22 @@
-def stock_picker()
+def stock_picker(prices)
+    max_profit = 0
+    buy_day = 0
+    sell_day = 0
 
+    prices.each_with_index do |price, day|
+        prices_two = prices[day..-1]
+        prices_two.each_with_index do |price_two, day_two|
+            current_profit = price_two - price
+            if current_profit > max_profit
+                max_profit = price_two - price
+                buy_day = day
+                sell_day = day_two + day
+            end
+        end
+    end
+
+    [buy_day, sell_day]
 end
+
+p stock_picker([17,3,6,9,15,8,6,1,10])
+#   => [1,4]  # for a profit of $15 - $3 == $12
